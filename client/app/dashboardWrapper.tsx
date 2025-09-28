46603379b11858c51fef6a7dd370c42a65d36ea8
Abhishek-17H <abhishekhegaje1702@gmail.com>
2025-09-28 09:54:10 +0530

    added redux toolkit and developed sidebar

--- client/app/dashboardWrapper.tsx
+++ client/app/dashboardWrapper.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
   <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-    {/* Sidebar */}
-    Sidebar
+    <Sidebar/>
     <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50`}>
            <Navbar/>
            {children}
